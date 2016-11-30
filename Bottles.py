3a6fc1955a2484ef03f13052b1cfc246bc7b772f
arotena <albertoss27@hotmail.com>
2016-11-30 12:09:27 +0100

    nueva version

--- Bottles.py
+++ Bottles.py
@@ -1,4 +1,3 @@
-from container import CONTAINER_NUMBER
 class Bottles:
 
     def song(self):
@@ -11,19 +10,47 @@ class Bottles:
         return o
 
     def verse(self, number = None):
-        actual = CONTAINER_NUMBER(number)
-        menos1=CONTAINER_NUMBER(number-1)
-        if number == 0:
-            return """No more bottles of beer on the wall, no more bottles of beer.
-Go to the store and buy some more, 99 bottles of beer on the wall.
-"""		
+        bottle_number = BottleNumber(number)
+        next_bottle_number = BottleNumber(bottle_number.successor())
+        result = \
+            "{0} {1} of beer on the wall, ".format(bottle_number.quantity().capitalize(), bottle_number.container()) + \
+            "{0} {1} of beer.\n".format(bottle_number.quantity(), bottle_number.container()) + \
+            "{0}, ".format(bottle_number.action()) + \
+            "{0} {1} of beer on the wall.\n".format(next_bottle_number.quantity(), next_bottle_number.container())
+
+        return result
+        
+class BottleNumber:
+    def __init__(self, number):
+        self.number = number
+
+    def container(self):
+        if self.number == 1:
+            return "bottle"
         else:
-           return """{0} {1} of beer on the wall, {0} {1} of beer.
-Take {3} down and pass it around, {2} """.format(number,actual.container(), menos1.no_more(),actual.it_one()) + """{0} of beer on the wall.
-""".format(menos1.container())
-    
-    
+            return "bottles"
 
+    def quantity(self):
+        if self.number == 0:
+            return "no more"
+        else:
+            return str(self.number)
+        
+    def action(self):
+        if self.number == 0:
+            return "Go to the store and buy some more"
+        else:
+            return "Take {0} down and pass it around".format(self.pronoun())
 
+    def pronoun(self):
+        if self.number == 1:
+            return "it"
+        else:
+            return "one"
 
+    def successor(self):
+        if self.number == 0:
+            return 99
+        else:
+            return self.number - 1
         
